d6b38c4872f740d03a1b797a9332c498b0509472
calebmutisya <mutisyacalebmusau@gmail.com>
2024-12-30 22:24:24 +0300

    variables

---
+++ variable.js
@@ -0,0 +1,4 @@
+var greetings = "Hello World";
+var age = 20;
+var isAlive = true;
+console.log(greetings);
---
+++ variable.ts
@@ -0,0 +1,7 @@
+let greetings : string = "Hello World";
+let age : number = 20;
+let isAlive : boolean = true;
+
+console.log(greetings);
+
+export {}
